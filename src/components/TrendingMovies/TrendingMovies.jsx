06e6cbf43b40e3bbeeecde4ec91832dbb055096b
BorschVS <borschvadym@gmail.com>
2024-02-27 02:07:08 +0200

    add eslint-disable-next-line

--- src/components/TrendingMovies/TrendingMovies.jsx
+++ src/components/TrendingMovies/TrendingMovies.jsx
@@ -11,6 +11,7 @@ const TrendingMovies = () => {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     getTrendingMovies();
   }, []);
 
