52a8fc2c8aeffe2d8fc752d21cfb3ade4c913100
BorschVS <129619261+BorschVS@users.noreply.github.com>
2024-02-28 23:10:54 +0000

    Deploying to gh-pages from @ BorschVS/goit-react-hw-05-movies@7cc51bb13366e9048081647175ee34dc910cf0be 🚀

---
+++ static/js/884.63a47c12.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[884],{2884:function(e,s,r){r.r(s);var i=r(2791),n=r(7689),t=r(1087),c=r(2881),u=r(184);s.default=function(){var e,s,r=(0,n.TH)(),l=(0,i.useRef)(null!==(e=null===(s=r.state)||void 0===s?void 0:s.from)&&void 0!==e?e:"/");return(0,u.jsxs)("div",{children:[(0,u.jsx)(t.rU,{to:l.current,children:"Back"}),(0,u.jsx)(i.Suspense,{fallback:(0,u.jsx)(c.Z,{}),children:(0,u.jsx)(n.j3,{})})]})}}}]);
+//# sourceMappingURL=884.63a47c12.chunk.js.map
